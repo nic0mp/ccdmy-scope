c0333743e833f225c0b428e3325c2e4bdbfc7e09
nic0mp <nico.mp215@gmail.com>
2022-01-26 15:30:43 -0600

    notes update

--- scopeNotes.js
+++ scopeNotes.js
@@ -6,3 +6,13 @@ const logCitySkyline = () => {
 };
 console.log(logCitySkyline())
 // output: The stars over the Empire State Building in New York City
+
+// GLOBAL SCOPE
+const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+const stars = 'North Star';
+const callMyNightSky = () => {
+  return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
+};
+console.log(callMyNightSky())
+// output:Night Sky: The Moon, North Star, and The Milky Way
