6e544caf5b94f8d91b20848a74fe2c071a91adc9
nic0mp <nico.mp215@gmail.com>
2022-01-25 23:18:52 -0600

    updates

---
+++ scopeNotes.js
@@ -0,0 +1,8 @@
+// Blocks and Scope
+const city = 'New York City';
+const logCitySkyline = () => {
+  let skyscraper = 'Empire State Building';
+  return 'The stars over the ' + skyscraper + ' in ' + city;
+};
+console.log(logCitySkyline())
+// output: The stars over the Empire State Building in New York City
